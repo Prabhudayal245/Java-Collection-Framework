32f8ddebfcc23d374533e198557415d8db8f9cde
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-26 19:16:53 +0530

    Add initial implementation of ArrayList example

---
+++ list/ArrayList/_01_Implementation.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class _01_Implementation {
+    public static void main(String[] args) {
+        // integer arraylist
+        ArrayList<Integer> list1=new ArrayList<>();  // initialize ArrayList 
+
+        // string arraylist
+        ArrayList<String> list2=new ArrayList<>();
+
+        
+
+    }
+}
