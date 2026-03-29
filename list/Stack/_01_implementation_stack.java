0b9dd86abcdb91cca74d95af1a6e602963a83caa
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-29 23:07:08 +0530

    Add initial implementation of Stack with example usage

---
+++ list/Stack/_01_implementation_stack.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class _01_implementation_stack{
+    public static void main(String[] args) {
+        // initialization of stack
+        Stack<Integer> stack1=new Stack<>();
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+        stack1.push(5);
+
+        System.out.println(stack1);
+    }
+}
