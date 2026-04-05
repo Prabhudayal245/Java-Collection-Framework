61fee5f3d519e2b4e4dcdbae7a63e7341f4e8ac7
Prabhudayal <prabhudayaly005@gmail.com>
2026-04-05 18:38:11 +0530

    Add how to do iteration in hashmap with the example

---
+++ Hashing/Map/Hashmap/iteration_hashmap.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class iteration_hashmap {
+    public static void main(String[] args) {
+        // creation 
+        HashMap<String,Integer> hm=new HashMap<>();
+
+        // insertion
+        hm.put("india",150);
+        hm.put("nepal",70);
+        hm.put("china",140);        
+        hm.put("sri lanka",110);        
+        hm.put("japan",50);        
+        hm.put("africa",100);   
+        
+        // iteration
+        Set<String> keys=hm.keySet();
+
+        // set of keys
+        System.out.println(keys);
+
+        // for 
+        for (String k : keys) {
+            System.out.println("Keys="+k+",Value="+hm.get(k));
+        }
+
+        // finding the max value through iteration
+        int max=0;
+        for(String s:keys){
+            if(max<hm.get(s)){
+                max=hm.get(s);
+            }
+        }
+        System.out.println("maximum:"+max);
+    }
+}
