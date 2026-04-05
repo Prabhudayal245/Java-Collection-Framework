17606f24da2d9e5b80d86e16508e3b86f99c28f5
Prabhudayal <prabhudayaly005@gmail.com>
2026-04-05 18:36:08 +0530

    Add initial implementation of HashMap with example usage

---
+++ Hashing/Map/Hashmap/initialization_Hashmap.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class initialization_Hashmap {
+    public static void main(String[] args) {
+        // creation
+        HashMap<String,Integer> hm=new HashMap<>();
+
+        // HashMap<KeyType,ValueType> hashmap_name=new HashMap<>();
+    }
+}
