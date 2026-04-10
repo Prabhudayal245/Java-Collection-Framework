739715e6690060dbbbbb0822c434db6fdddddcad
Prabhudayal <prabhudayaly005@gmail.com>
2026-04-10 23:59:54 +0530

    Add initial implementation of HashSet with basic usage example

---
+++ Hashing/Set/HashSet/Implementation.java
@@ -0,0 +1,14 @@
+// Internally,The HashSet is implemented by using HashMap
+// It cannot contains duplicate value
+// it is unorderes
+// the null is allowed 
+// the values which are store in set is actually a Key in a HashMap 
+import java.util.HashSet;
+
+public class Implementation{
+    public static void main(String[] args){
+        // initialize the HashSet
+        HashSet<Integer> set=new HashSet<>();
+
+    }
+}
