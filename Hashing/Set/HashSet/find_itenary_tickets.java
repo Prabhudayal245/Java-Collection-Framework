f8e57a32a50b7b31ca718bc81d4eb3689339452c
Prabhudayal <prabhudayaly005@gmail.com>
2026-04-12 22:55:22 +0530

    Add implementation of itinerary tickets using HashMap

---
+++ Hashing/Set/HashSet/find_itenary_tickets.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class find_itenary_tickets {
+    public static String get_start(HashMap<String,String> map){
+        HashMap<String,String> revmap=new HashMap<>();
+
+        for(String key:map.keySet()){
+            revmap.put(map.get(key), key);
+        }
+
+        for(String key:map.keySet()){
+            if(!revmap.containsKey(key)){
+                return key;
+            }
+        }
+
+        return null;
+    }
+    public static void main(String[] args) {
+        HashMap<String,String> map=new HashMap<>();
+
+        map.put("Chennai", "Bengaluru");
+        map.put("Mumbai", "Delhi");
+        map.put("Goa", "Chennai");
+        map.put("Delhi", "Goa");
+
+        String start=get_start(map);
+        System.out.print(start);
+        for(String Keys:map.keySet()){
+            if(map.containsKey(start)){
+                System.out.print("->"+map.get(start));
+                start=map.get(start);
+            }
+        }
+    }
+}
